527436fe7a7dfa58b9405d852c1eaff55e9a68b7
ohto <hirohito.ohto@gmail.com>
2026-01-10 15:06:40 +0900

    feat: groupingロジックのscrapingへの実装

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pandas as pd
 from dateutil import tz
 from src.core.formatter import format_data
+from src.core.grouping import group_by_properties
 from src.core.scraping_manager import Scraper
 
 jst = tz.gettz("Asia/Tokyo")
@@ -12,49 +13,34 @@ now_jst = datetime.now(jst)
 yyyymmdd = int(now_jst.strftime("%Y%m%d"))
 
 
-def scrape(case_name: str, max_page: int = 10000) -> pd.DataFrame:
-    """suumoのベースURLに対してスクレイピングを行い、結果をcsvファイルに保存する
-
-    Args:
-        case_name (str): setting.ymlのtargetに対応するキー
-        max_page (int): スクレイピングするページ数
-
-    Returns:
-        pd.DataFrame: スクレイピング結果
-    """
-    scraper = Scraper(case_name)
-    scraper.extract_page(max_page)
-    return pd.DataFrame(scraper.data_all)
+def _output_csv(df: pd.DataFrame, dir_path: str) -> None:
+    os.makedirs(dir_path, exist_ok=True)
+    filename = os.path.join(dir_path, f"{yyyymmdd}.csv")
+    df.to_csv(filename, index=False)
 
 
-def format(df: pd.DataFrame) -> pd.DataFrame:
-    """スクレイピング結果を整形する
-
-    Args:
-        df (pd.DataFrame): スクレイピング結果
-
-    Returns:
-        pd.DataFrame: 整形後のスクレイピング結果
-    """
-    return format_data(df)
-
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 2:
         print("Usage: python scrape.py <case_name>")
         sys.exit(1)
     case_name = sys.argv[1]
 
     # スクレイピング
-    df_raw = scrape(case_name)
-    dir_raw = f"data/{case_name}/lake"
-    os.makedirs(dir_raw, exist_ok=True)
-    filename_lake = os.path.join(dir_raw, f"{yyyymmdd}.csv")
-    df_raw.to_csv(filename_lake, index=False)
+    scraper = Scraper(case_name)
+    scraper.extract_page(max_page=10000)
+    df_raw = pd.DataFrame(scraper.data_all)
+    _output_csv(df_raw, f"data/{case_name}/lake")
+
     # スクレイピング結果を整形
-    df_formatted = format(df_raw)
-    dir_formatted = f"data/{case_name}/formatted"
-    os.makedirs(dir_formatted, exist_ok=True)
-    filename_lake = os.path.join(dir_formatted, f"{yyyymmdd}.csv")
-    df_formatted.to_csv(filename_lake, index=False)
+    df_formatted = format_data(df_raw)
+    _output_csv(df_formatted.sort_values("id"), f"data/{case_name}/formatted")
+
     # grouping処理を行う
+    df_grouped = group_by_properties(
+        df_formatted, group_cols=["name", "price", "age", "layout", "area"]
+    )
+    _output_csv(df_grouped.sort_values("id"), f"data/{case_name}/grouped")
+
+
+if __name__ == "__main__":
+    main()
